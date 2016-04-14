6beef3f23ad031d75c185c311df0a1d1be89ddd6
samo92 <damianmaciel92@gmail.com>
2016-04-13 20:43:19 -0500

    MapView

--- app/src/main/java/samo92/mx/appbike/MapviewActivity.java
+++ app/src/main/java/samo92/mx/appbike/MapviewActivity.java
@@ -3,6 +3,8 @@ package samo92.mx.appbike;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.mapbox.mapboxsdk.maps.MapView;
+
 /**
  * Created by samo92 on 13/04/16.
  */
@@ -11,7 +13,10 @@ public class MapviewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapview);
 
-        findViewById(R.id.mapview);
+        // Create a mapView
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
     }
 }
