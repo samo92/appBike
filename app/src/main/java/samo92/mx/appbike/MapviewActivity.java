f3e85ca25bb8e4cd3bdbee72ab86b96860abd82d
samo92 <damianmaciel92@gmail.com>
2016-04-13 20:09:27 -0500

    accessToken

---
+++ app/src/main/java/samo92/mx/appbike/MapviewActivity.java
@@ -0,0 +1,17 @@
+package samo92.mx.appbike;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by samo92 on 13/04/16.
+ */
+public class MapviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        findViewById(R.id.mapview);
+    }
+}
