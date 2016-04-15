4cb2504d2644a6942dff858e59bf02547dbd119b
samo92 <damianmaciel92@gmail.com>
2016-04-15 01:30:27 -0500

    create an object call MapBoxMap

--- app/src/main/java/samo92/mx/appbike/MapviewActivity.java
+++ app/src/main/java/samo92/mx/appbike/MapviewActivity.java
@@ -3,7 +3,13 @@ package samo92.mx.appbike;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 /**
  * Created by samo92 on 13/04/16.
@@ -18,5 +24,30 @@ public class MapviewActivity extends AppCompatActivity {
         // Create a mapView
         MapView mapView = (MapView) findViewById(R.id.mapview);
         mapView.onCreate(savedInstanceState);
+
+
+        //MapBoxMap object allows you to change styles and interact with your map.
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+
+                // Set map style
+                mapboxMap.setStyleUrl(Style.MAPBOX_STREETS);
+
+                // Set the camera's starting position
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(new LatLng(41.885, -87.679)) // set the camera's center position
+                        .zoom(12)  // set the camera's zoom level
+                        .tilt(20)  // set the camera's tilt
+                        .build();
+
+                // Move the camera to that position
+                mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+
+            );
+
+        }
+
+
     }
-}
