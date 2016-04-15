7b3d11246fdc47f0c8467df2a4ed93ff68b98a85
samo92 <damianmaciel92@gmail.com>
2016-04-15 02:08:16 -0500

    Add markes to map

--- app/src/main/java/samo92/mx/appbike/MapviewActivity.java
+++ app/src/main/java/samo92/mx/appbike/MapviewActivity.java
@@ -3,6 +3,7 @@ package samo92.mx.appbike;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.constants.Style;
@@ -16,13 +17,17 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
  */
 public class MapviewActivity extends AppCompatActivity {
 
+    // Create a mapView
+    private MapView mapView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapview);
 
         // Create a mapView
-        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView = (MapView) findViewById(R.id.mapview);
         mapView.onCreate(savedInstanceState);
 
 
@@ -36,18 +41,104 @@ public class MapviewActivity extends AppCompatActivity {
 
                 // Set the camera's starting position
                 CameraPosition cameraPosition = new CameraPosition.Builder()
-                        .target(new LatLng(41.885, -87.679)) // set the camera's center position
+
+                        //Chicago position
+                        //.target(new LatLng(41.885, -87.679)) // set the camera's center position
+
+                        //CU position
+                        .target(new LatLng(19.331576, -99.184483)) // set the camera's center position
                         .zoom(12)  // set the camera's zoom level
-                        .tilt(20)  // set the camera's tilt
+                        .tilt(20)  // set the camera's tilt (tilt==Inclinacion)
                         .build();
 
                 // Move the camera to that position
                 mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+            //Creating Markers
+
+
+            //The Office
+            mapboxMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(19.326350, -99.181819))    //Office
+                    .title("Here I am")
+                    .snippet("Welcome to my office."));
+
+            //Parking
+            mapboxMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(19.332345, -99.184451))
+                    .title("Hello World!")
+                    .snippet("Welcome to my marker."));
+
+            mapboxMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(19.332022, -99.190577))
+                    .title("Hello World!")
+                    .snippet("Welcome to my marker."));
+
+            mapboxMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(19.332790, -99.181875))
+                    .title("Hello World!")
+                    .snippet("Welcome to my marker."));
+
+            mapboxMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(19.326341, -99.182306))
+                    .title("Hello World!")
+                    .snippet("Welcome to my marker."));
+
+            //Bike
+            mapboxMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(19.333506, -99.179225))
+                    .title("Hello World!")
+                    .snippet("Welcome to my marker."));
+
+            mapboxMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(19.333157, -99.182261))
+                    .title("Hello World!")
+                    .snippet("Welcome to my marker."));
+
+            mapboxMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(19.331907, -99.187693))
+                    .title("Hello World!")
+                    .snippet("Welcome to my marker."));
+
+            mapboxMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(19.333448, -99.191377))
+                    .title("Hello World!")
+                    .snippet("Welcome to my marker."));
+
             }
 
         });
-
-
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
 }
 
